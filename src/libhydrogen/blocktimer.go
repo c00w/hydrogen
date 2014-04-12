4afaddd0ed31b846a391aa42f39533e05bf4d106
Colin Rice <colin@daedrum.net>
2014-04-12 16:35:22 -0400

    Add a blocktimer to handle tau calculations

---
+++ src/libhydrogen/blocktimer.go
@@ -0,0 +1,56 @@
+package libhydrogen
+
+import (
+    "time"
+    "sync"
+)
+
+func NewBlockTimer(tau time.Duration, lasttime time.Time) *BlockTimer {
+    b := &BlockTimer{tau, lasttime, nil, make(chan struct{}), &sync.RWMutex{}}
+    b.setupTimer()
+    return b
+}
+
+type BlockTimer struct {
+    tau time.Duration
+    lasttime time.Time
+    currenttimer *time.Timer
+    firingchan chan struct{}
+    lock *sync.RWMutex
+}
+
+func (b *BlockTimer) Chan() chan struct{} {
+    b.lock.RLock()
+    defer b.lock.RUnlock()
+    return b.firingchan
+}
+
+func (b *BlockTimer) IncreaseTau() {
+    b.lock.Lock()
+    b.tau = time.Duration(b.tau.Nanoseconds()*11/10)
+    b.currenttimer.Stop()
+    b.setupTimer()
+    b.lock.Unlock()
+}
+
+func (b *BlockTimer) DecreaseTau() {
+    b.lock.Lock()
+    b.tau = time.Duration(b.tau.Nanoseconds()*10/11)
+    b.currenttimer.Stop()
+    b.setupTimer()
+    b.lock.Unlock()
+}
+
+func (b *BlockTimer) setupTimer() {
+    wait := b.lasttime.Sub(time.Now())
+    wait = time.Duration(wait.Nanoseconds() % b.tau.Nanoseconds())
+    b.currenttimer = time.AfterFunc(wait, b.timerFired)
+}
+
+func (b *BlockTimer) timerFired() {
+    b.lock.Lock()
+    b.lasttime = b.lasttime.Add(b.tau)
+    b.firingchan <- struct{}{}
+    b.setupTimer()
+    b.lock.Unlock()
+}
