1a63f1db04aac13c76bad841e5fedda121dcfba7
Colin Rice <colin@daedrum.net>
2014-04-18 20:15:33 -0400

    go fmt util

--- src/util/util.go
+++ src/util/util.go
@@ -2,26 +2,23 @@
 package util
 
 import (
-    "crypto/ecdsa"
-    "crypto/elliptic"
-    "crypto/rand"
-    "crypto/sha512"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha512"
 )
 
 func KeyString(k *ecdsa.PrivateKey) string {
-    s := sha512.New()
-    s.Write(k.X.Bytes())
-    s.Write(k.Y.Bytes())
-    return string(s.Sum(nil))
+	s := sha512.New()
+	s.Write(k.X.Bytes())
+	s.Write(k.Y.Bytes())
+	return string(s.Sum(nil))
 }
 
 func GenKey() *ecdsa.PrivateKey {
-    k, e := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
-    if e != nil {
-        panic(e)
-    }
-    return k
+	k, e := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	if e != nil {
+		panic(e)
+	}
+	return k
 }
-
-
-
